82993f4f3cbfbf49692997b4f92e91c6fb3d78e3
HAP-203 <hap-203@HAP-203s-MacBook-Air.local>
2021-01-16 00:25:37 +0530

    Added custom user

--- accounts/custom_auth_token.py
+++ accounts/custom_auth_token.py
@@ -1,21 +1,20 @@
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
-from rest_framework import status
+
+# from rest_framework import status
+
 # from dukaan.common.base_view import BaseView
 from .serializers import UserSerializer
 
+
 class CustomAuthToken(ObtainAuthToken):
-    base_view = BaseView()
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(
             data=request.data, context={"request": request}
         )
         if not serializer.is_valid():
-            pass
-            # return self.base_view.serializer_error_response(
-            #     "Authentication failed ", serializer.errors
-            # )
+            return Response("Authentication failed ", serializer.errors)
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
--- dukaan/wsgi.py
+++ dukaan/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dukaan.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dukaan.settings")
 
 application = get_wsgi_application()
